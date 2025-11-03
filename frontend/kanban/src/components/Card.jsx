9905894a577d435c7b90163ba1201ed5b6569a76
Berangere Br <brunberangere@gmail.com>
2025-11-03 19:00:11 +0100

    btn supprimer amélioré

--- frontend/kanban/src/components/Card.jsx
+++ frontend/kanban/src/components/Card.jsx
@@ -1,12 +1,20 @@
-
 import '../styles/card.scss';
+import deleteIcon from '../assets/icons/delete.png';
+import deleteIconHover from '../assets/icons/deleteHover.png'
+import { useState } from 'react';
 
 function Card({ card, onDelete }) {
+    const [deleteCard, setdeleteCard] = useState(false);
+
     return (
         <div className="card">
-            <h2>{card.name}</h2>
-            <p className='description'>{card.description}</p>
-            <button onClick={() => onDelete(card.id)} className="btn btn-card">supprimer</button>
+            <div className='card-supr'>
+                <button onClick={() => onDelete(card.id)} className="card-btn-supr" onMouseOver={() => setdeleteCard(true)} onMouseLeave={() => setdeleteCard(false)}>
+                    <img src={deleteCard ? deleteIconHover : deleteIcon} alt='delete' width='20px' />
+                </button>
+            </div>
+            <h2 className='card-name'>{card.name}</h2>
+            <p className='card-description'>{card.description}</p>
         </div>
     )
 }
